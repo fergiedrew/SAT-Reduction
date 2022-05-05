8431d40f56c0f222ecaf46e85339ae0f1095d624
fergiedrew <55263375+fergiedrew@users.noreply.github.com>
2022-05-04 21:12:11 -0500

    Starter file / Problem desc.

---
+++ satreduction.py
@@ -0,0 +1,15 @@
+# Make a solver that reduces grid problem #1 from:
+# https://udallas.brightspace.com/d2l/le/content/47383/viewContent/504108/View
+
+# Problem Description:
+
+# Given a rectangular grid with some of the squares 
+# blocked off, is there a tour that will visit all of the remaining squares exactly once? 
+# Figure A to the right is such an example input. The answer is "Yes" for this input, as shown 
+# by the blue curve below in Figure B. The input below, in Figure C, yields a "No" answer for this problem.
+# Input will be a string in this format: Height Width block block block ...
+# For example: 3 5 (5,1) (2,2) (3,2) would be the input for Figure A.
+# (There will be spaces between, and no spaces within.)
+
+
+
